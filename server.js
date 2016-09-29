91ff05a2f1bf7ede5d0f277d2c9d0fd610fa4cef
kbbushman <k.bushman@yahoo.com>
2016-09-29 13:58:34 -0700

    renders test data to home page

--- server.js
+++ server.js
@@ -59,10 +59,6 @@ app.get('/api/projects', function index(req, res) {
 });
 
 
-
-
-
-
 // Server
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
