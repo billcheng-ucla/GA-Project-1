c18d0d5248a0221d91f722ec94c9339a2daaf94a
kbbushman <k.bushman@yahoo.com>
2016-09-29 10:33:35 -0700

    adds ejs file structure

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,10 @@ var allProjects = [];
 
 $(document).ready(function(){
 
+// EJS setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 
 }); // End Document Ready
